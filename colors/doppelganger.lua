8552c7ab92c306c3f6cbdd64d99b02a8ad00b5fc
unknown <thecguy73@gmail.com>
2025-06-25 21:08:46 +0200

    adding the source of the plugin

---
+++ colors/doppelganger.lua
@@ -0,0 +1,1 @@
+require("doppelganger").load() 
---
+++ lua/doppelganger/init.lua
@@ -0,0 +1,55 @@
+local M = {}
+
+function M.load()
+  vim.cmd("highlight clear")
+  if vim.fn.exists("syntax_on") then
+    vim.cmd("syntax reset")
+  end
+  vim.o.termguicolors = true
+  vim.g.colors_name = "doppelganger"
+
+  -- Palette Zenburn
+  local palette = {
+    bg         = "#3f3f3f",
+    fg         = "#dcdccc",
+    comment    = "#7f9f7f",
+    keyword    = "#f0dfaf",
+    string     = "#cc9393",
+    func       = "#efef8f",
+    variable   = "#dfaf8f",
+    constant   = "#dca3a3",
+    type       = "#dfdfbf",
+    cursorline = "#2b2b2b",
+    visual     = "#5f5f5f",
+    number     = "#8cd0d3",
+    error      = "#e37170",
+    warning    = "#dc8cc3",
+  }
+
+  -- Gruppi principali
+  vim.api.nvim_set_hl(0, "Normal",      { fg = palette.fg, bg = palette.bg })
+  vim.api.nvim_set_hl(0, "Comment",     { fg = palette.comment, italic = true })
+  vim.api.nvim_set_hl(0, "Keyword",     { fg = palette.keyword, bold = true })
+  vim.api.nvim_set_hl(0, "String",      { fg = palette.string })
+  vim.api.nvim_set_hl(0, "Function",    { fg = palette.func })
+  vim.api.nvim_set_hl(0, "Identifier",  { fg = palette.variable })
+  vim.api.nvim_set_hl(0, "Constant",    { fg = palette.constant })
+  vim.api.nvim_set_hl(0, "Type",        { fg = palette.type })
+  vim.api.nvim_set_hl(0, "Number",      { fg = palette.number })
+  vim.api.nvim_set_hl(0, "CursorLine",  { bg = palette.cursorline })
+  vim.api.nvim_set_hl(0, "Visual",      { bg = palette.visual })
+  vim.api.nvim_set_hl(0, "Error",       { fg = palette.error, bold = true })
+  vim.api.nvim_set_hl(0, "WarningMsg",  { fg = palette.warning, bold = true })
+  vim.api.nvim_set_hl(0, "LineNr",      { fg = "#5f7f5f", bg = palette.bg })
+  vim.api.nvim_set_hl(0, "CursorLineNr",{ fg = palette.keyword, bg = palette.cursorline, bold = true })
+  vim.api.nvim_set_hl(0, "NonText",     { fg = "#6f6f6f" })
+  vim.api.nvim_set_hl(0, "StatusLine",  { fg = palette.fg, bg = palette.cursorline })
+  vim.api.nvim_set_hl(0, "StatusLineNC",{ fg = palette.comment, bg = palette.cursorline })
+  vim.api.nvim_set_hl(0, "VertSplit",   { fg = palette.cursorline, bg = palette.bg })
+  vim.api.nvim_set_hl(0, "Pmenu",       { fg = palette.fg, bg = "#4f4f4f" })
+  vim.api.nvim_set_hl(0, "PmenuSel",    { fg = palette.bg, bg = palette.keyword })
+  vim.api.nvim_set_hl(0, "Search",      { fg = palette.bg, bg = palette.keyword, bold = true })
+  -- Puoi aggiungere altri gruppi secondo necessità
+end
+
+return M 
